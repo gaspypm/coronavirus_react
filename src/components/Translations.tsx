781b84c9e13524beb9e5acec358f0ca5617991d4
gaspypm <gaspypm@gmail.com>
2023-10-10 09:30:46 -0300

    import translation files to Translations.tsx

---
+++ src/components/Translations.tsx
@@ -0,0 +1,17 @@
+import enTranslations from "@translations/en.json";
+import frTranslations from "@translations/fr.json";
+import esTranslations from "@translations/es.json";
+import deTranslations from "@translations/de.json";
+import nlTranslations from "@translations/nl.json";
+import nbTranslations from "@translations/nb.json";
+
+const translations = {
+	en: enTranslations,
+	es: esTranslations,
+	fr: frTranslations,
+	de: deTranslations,
+	nl: nlTranslations,
+	nb: nbTranslations,
+};
+
+export default translations;
